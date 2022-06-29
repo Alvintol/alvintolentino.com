1f9bc78220655592d1e16a42984f5fc56ee8f74e
Alvin <alvin.tol@hotmail.com>
2022-06-28 20:17:54 -0600

    adds hardcoded top nav placeholders

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <header>
+      Header
+      <div>nameplate</div>
+      <div>skills</div>
+      <div>projects</div>
+      <div>contact</div>    
+      </header>
 
     </div>
   );
